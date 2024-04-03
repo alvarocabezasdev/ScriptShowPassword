5cff54493d820bde28f3ffce8dc55daca5dd737f
alvarocabezasdev <alvarocabezasdev@gmail.com>
2024-04-03 21:32:18 +0200

    Update ScriptWifiPassword.ps1

--- ScriptWifiPassword.ps1
+++ ScriptWifiPassword.ps1
@@ -1,5 +1,13 @@
-$wifiProfiles = netsh wlan show profiles | ForEach-Object {
-    $_.ToString().Split(":")[1].Trim()
-}
+$wifiProfiles = netsh wlan show profiles | Select-String -Pattern "All User Profile" | ForEach-Object { $_ -replace "^.*:\s*", "" }
 
-Write-Host $wifiProfiles
+foreach ($profile in $wifiProfiles) {
+    Write-Host "Nombre de la Red (SSID):" $profile
+    $wifiDetails = netsh wlan show profile name="$profile" key=clear
+    $wifiKey = ($wifiDetails | Select-String "Contenido de la clave").ToString().Split(":")[1].Trim()
+    if ($wifiKey -ne $null) {
+        Write-Host "Contraseña:" $wifiKey
+    } else {
+        Write-Host "Contraseña: No Disponible"
+    }
+    Write-Host "--------------------------------"
+}
