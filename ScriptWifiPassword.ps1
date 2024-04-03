8bc47dc116a274e2f6558bc648bf4a82316d9e1d
alvarocabezasdev <alvarocabezasdev@gmail.com>
2024-04-03 21:29:35 +0200

    Update ScriptWifiPassword.ps1

--- ScriptWifiPassword.ps1
+++ ScriptWifiPassword.ps1
@@ -1,26 +1,5 @@
-# Listar todos los perfiles de red Wi-Fi
-$profiles = netsh wlan show profiles | Select-String -Pattern "All User Profile" | ForEach-Object {
-    $_ -replace "^.*:\s*", ""
+$wifiProfiles = netsh wlan show profiles | Select-String -Pattern "All User Profile" | ForEach-Object {
+    $_.ToString().Split(":")[1].Trim()
 }
 
-foreach ($profile in $profiles) {
-    # Mostrar el nombre del perfil
-    Write-Output "Nombre de la red (SSID): $profile"
-    
-    # Intentar mostrar la contraseña del perfil. Si no está disponible, captura el error y muestra un mensaje.
-    try {
-        $key = (netsh wlan show profile name="$profile" key=clear | Select-String -Pattern "Key Content" | ForEach-Object {
-            $_ -replace "^.*:\s*", ""
-        })
-        if ($key -eq $null) {
-            Write-Output "Contraseña: No disponible o red abierta"
-        } else {
-            Write-Output "Contraseña: $key"
-        }
-    } catch {
-        Write-Output "No se pudo obtener la información de la contraseña para $profile"
-    }
-    
-    # Espaciado entre perfiles
-    Write-Output ""
-}
+Write-Host $wifiProfiles
