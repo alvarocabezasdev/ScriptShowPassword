fb3d78fc4c6fc07c17c5e65ece223f11b85bea35
alvarocabezasdev <45815034+alvarocabezasdev@users.noreply.github.com>
2024-04-03 19:17:55 +0200

    Add files via upload

---
+++ ScriptWifiPassword.ps1
@@ -0,0 +1,15 @@
+$profilesOutput = Invoke-Expression "netsh wlan show profile"
+
+$profiles = $profilesOutput -split "`n"
+foreach ($profile in $profiles) {
+    if (-not [string]::IsNullOrWhiteSpace($profile)) {
+        $profileName = $profile -replace "Perfil\s+:\s+", ""
+        
+        Write-Output "Procesando perfil: $profileName"
+        
+        $profileDetails = Invoke-Expression "netsh wlan show profile name=`"$profileName`" key=clear"
+        
+        Write-Output $profileDetails
+        
+    }
+}
