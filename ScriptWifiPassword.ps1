09f5b46f629a34430b1afb6f7ce40e3f78836eb6
alvarocabezasdev <alvarocabezasdev@gmail.com>
2024-04-03 21:34:29 +0200

    Update ScriptWifiPassword.ps1

--- ScriptWifiPassword.ps1
+++ ScriptWifiPassword.ps1
@@ -1,13 +1,19 @@
-$wifiProfiles = netsh wlan show profiles | Select-String -Pattern "All User Profile" | ForEach-Object { $_ -replace "^.*:\s*", "" }
+$profilesOutput = Invoke-Expression "netsh wlan show profiles"
 
-foreach ($profile in $wifiProfiles) {
-    Write-Host "Nombre de la Red (SSID):" $profile
-    $wifiDetails = netsh wlan show profile name="$profile" key=clear
-    $wifiKey = ($wifiDetails | Select-String "Contenido de la clave").ToString().Split(":")[1].Trim()
-    if ($wifiKey -ne $null) {
-        Write-Host "Contraseña:" $wifiKey
-    } else {
-        Write-Host "Contraseña: No Disponible"
+# Filtrar y extraer solo los nombres de los perfiles
+$profiles = $profilesOutput -split "`n" | Where-Object { $_ -match "Todos los perfiles de usuario\s+:\s+(.+)" }
+
+foreach ($profile in $profiles) {
+    # Extraer el nombre del perfil utilizando la captura de grupo en -match
+    if ($profile -match "Todos los perfiles de usuario\s+:\s+(.+)") {
+        $profileName = $matches[1]
+        
+        Write-Output "Procesando perfil: $profileName"
+        
+        # Utilizar correctamente las comillas para envolver el nombre del perfil en el comando
+        $profileDetailsCmd = "netsh wlan show profile name=`"$profileName`" key=clear"
+        $profileDetails = Invoke-Expression $profileDetailsCmd
+        
+        Write-Output $profileDetails
     }
-    Write-Host "--------------------------------"
 }
