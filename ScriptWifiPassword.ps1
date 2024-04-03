65941cfc1b2cb39fa01269442bce8fdf4a6d70d9
alvarocabezasdev <alvarocabezasdev@gmail.com>
2024-04-03 21:14:07 +0200

    Update ScriptWifiPassword.ps1

--- ScriptWifiPassword.ps1
+++ ScriptWifiPassword.ps1
@@ -1,15 +1,1 @@
-$profilesOutput = Invoke-Expression "netsh wlan show profile"
-
-$profiles = $profilesOutput -split "`n"
-foreach ($profile in $profiles) {
-    if (-not [string]::IsNullOrWhiteSpace($profile)) {
-        $profileName = $profile -replace "Perfil\s+:\s+", ""
-        
-        Write-Output "Procesando perfil: $profileName"
-        
-        $profileDetails = Invoke-Expression "netsh wlan show profile name=\"$profileName\" key=clear" 
-
-        Write-Output $profileDetails
-        
-    }
-}
+(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
